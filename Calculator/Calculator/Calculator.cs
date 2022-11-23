f718855970a0e2578cd7312c5235bc8d087abf2c
4UPanElektryk <80508394+4UPanElektryk@users.noreply.github.com>
2022-11-23 08:48:05 +0100

    simplified

--- Calculator/Calculator/Calculator.cs
+++ Calculator/Calculator/Calculator.cs
@@ -13,22 +13,20 @@ namespace DecToBinAndBinToDec
                 if (answer == "b")
                 {
                     Console.Write("Insert Binary Number: ");
-                    string Binaries = Console.ReadLine();
-                    int Binary = int.Parse(Binaries);
+                    int Binary = int.Parse(Console.ReadLine(););
                     Console.WriteLine("This number in Decimal equals: " + Convert.ToInt32(Binaries, 2));    
                     break;
                 }
                 else if (answer == "d")
                 {
                     Console.Write("Insert Decimal Number: ");
-                    string input = Console.ReadLine();
-                    int Decimal = int.Parse(input);
+                    int Decimal = int.Parse(Console.ReadLine(););
                     Console.WriteLine("This number in Binary equals: " + Convert.ToString(Decimal, 2));
                     break;
                 }
                 else
                 {
-                    Console.WriteLine("Blunder in the Matrix. Try again!");
+                    Console.WriteLine("Incorrect Mode selected");
                 }
             }
             Console.ReadKey();
