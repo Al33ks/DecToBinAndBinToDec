2a2aecc7b688a0bda000adffb692e152ad01c8f5
4UPanElektryk <80508394+4UPanElektryk@users.noreply.github.com>
2022-11-23 08:41:38 +0100

    fixed and simplified mode selection

--- Calculator/Calculator/Calculator.cs
+++ Calculator/Calculator/Calculator.cs
@@ -7,27 +7,29 @@ namespace DecToBinAndBinToDec
         static void Main(string[] args)
         {
             Console.WriteLine("Swap Binary to Decimal (B), or Decimal to Binary (D)?");
-            string answer = Console.ReadLine();
-            
-
-            if (answer == "B" || answer == "b")
+            while(true)
             {
-                Console.Write("Insert Binary Number: ");
-                string Binaries = Console.ReadLine();
-                int Binary = int.Parse(Binaries);
-                Console.WriteLine("This number in Decimal equals: " + Convert.ToInt32(Binaries, 2));
-                
-            }
-            else if (answer == "D" || answer == "d")
-            {
-                Console.Write("Insert Decimal Number: ");
-                string input = Console.ReadLine();
-                int Decimal = int.Parse(input);
-                Console.WriteLine("This number in Binary equals: " + Convert.ToString(Decimal, 2));
-            }
-            else
-            {
-                Console.WriteLine("Blunder in the Matrix. Try again!");
+                string answer = Console.ReadLine().ToLower();
+                if (answer == "b")
+                {
+                    Console.Write("Insert Binary Number: ");
+                    string Binaries = Console.ReadLine();
+                    int Binary = int.Parse(Binaries);
+                    Console.WriteLine("This number in Decimal equals: " + Convert.ToInt32(Binaries, 2));    
+                    break;
+                }
+                else if (answer == "d")
+                {
+                    Console.Write("Insert Decimal Number: ");
+                    string input = Console.ReadLine();
+                    int Decimal = int.Parse(input);
+                    Console.WriteLine("This number in Binary equals: " + Convert.ToString(Decimal, 2));
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Blunder in the Matrix. Try again!");
+                }
             }
             Console.ReadKey();
         }
